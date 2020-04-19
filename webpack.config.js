720b3cef4790f85681f0b1d2ac1f898075e13b05
jacob-wong <wong.jacob@gmail.com>
2020-04-19 21:13:43 +1000

    using webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, '/dist/'),
+    inline: true,
+    host: 'localhost',
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+
+};
